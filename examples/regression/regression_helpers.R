cb5e0940804399740a02a43968f0c02fb588630d
Shane Kercheval <shane.kercheval@gmail.com>
2020-11-11 10:04:42 -0800

    regression

--- examples/regression/regression_helpers.R
+++ examples/regression/regression_helpers.R
@@ -58,3 +58,97 @@ plot_regression_variance_explained <- function(.regression_results) {
                                   "; Adjusted R-Squared: ", round(adjusted_r_squared, 3),")"))
     )
 }
+
+#' Actual vs. Predicted plot
+#'
+#' @param model model
+#'
+#' @importFrom magrittr "%>%"
+#' @importFrom ggplot2 geom_line aes geom_smooth labs
+#' @importFrom stringr str_remove_all
+#' @export
+plot_actual_vs_predicted <- function(model) {
+    actual_variable <- str_remove_all(string=as.character(model$terms)[2], pattern = '`')
+    
+    data.frame(actual=model$model[[actual_variable]],
+               pred=model$fitted.values) %>%
+        ggplot(aes(x=pred, y=actual)) +
+        geom_point(alpha=0.3) +
+        #rt_explore_plot_scatter(variable='actual', comparison_variable='pred') +
+        geom_line(aes(x=actual, y=actual), color='red') +
+        geom_smooth(method = 'auto') +
+        labs(title="Actual vs Predicted",
+             y=paste0("Actual (`", actual_variable,"`)"),
+             x="Model Prediction",
+             caption="Red line indicates perfect prediction.\nBlue line represets pattern of Prediction vs `", actual_variable,"`")
+}
+
+#' Residual vs. Predicted plot
+#'
+#' @param model model
+#'
+#' @importFrom magrittr "%>%"
+#' @importFrom ggplot2 geom_hline geom_smooth labs
+#' @export
+plot_residual_vs_predicted <- function(model) {
+    actual_variable <- as.character(model$terms)[2]
+    data.frame(resid=model$residuals,
+               pred=model$fitted.values) %>%
+        ggplot(aes(x=pred, y=resid)) +
+        geom_point(alpha=0.3) +
+        geom_hline(yintercept = 0, color='red') +
+        geom_smooth(method = 'auto') +
+        labs(title='Residual vs Predicted (a.k.a Fitted)',
+             subtitle = 'Residual = Actual - Prediction; a positive Residual indicates the model is under-predicting.',
+             y='Residual',
+             x='Model Prediction',
+             caption='Red line indicates perfect prediction (no residual).\nBlue line represets pattern of Residual vs Predicted (i.e. pattern that is not captured by the model).')
+}
+
+#' Residual vs. Predicted plot
+#'
+#' @param model model
+#' @param predictor the predictor i.e. variable to use
+#' @param dataset the original dataset
+#'
+#' @importFrom magrittr "%>%"
+#' @importFrom ggplot2 geom_hline geom_smooth labs
+#' @importFrom modelr add_residuals
+#' @export
+plot_residual_vs_variable <- function(model, predictor, dataset) {
+    
+    # need to do this because the regression (lm) automatically removes NAs
+    dataset <- dataset[complete.cases(dataset), ]
+    
+    stopifnot(nrow(model$model) == nrow(dataset))
+    
+    transformed_columns <- base::setdiff(colnames(model$model), colnames(dataset))
+    
+    if(length(transformed_columns) > 0) {
+        dataset <- cbind(dataset, model$model[, transformed_columns])
+    }
+    
+    dataset <- dataset %>% modelr::add_residuals(model)
+    
+    if(is.numeric(dataset[[predictor]])) {
+        
+        dataset %>%
+            ggplot(aes(x=!!sym(predictor), y=resid)) +
+            geom_point() +
+            # geom_point(alpha = 0.3) +
+            geom_hline(yintercept = 0, color='red') +
+            geom_smooth(method = 'auto') +
+            labs(title=paste0("Residual vs Predictor (`", predictor,"`)"),
+                 subtitle = "Residual = Actual - Prediction; a positive Residual indicates the model is under-predicting.",
+                 y="Residual",
+                 x=paste0("Predictor (`", predictor,"`)"),
+                 caption="Red line indicates perfect prediction (no residual).\nBlue line represets pattern of Residual vs Predicted (i.e. pattern that is not captured by the model).")
+    } else {
+        dataset %>%
+            rt_explore_plot_boxplot(variable='resid', comparison_variable=predictor) +
+            labs(title=paste0("Residual vs Predictor (`", predictor,"`)"),
+                 subtitle = 'Residual = Actual - Prediction; a positive Residual indicates the model is under-predicting.',
+                 y='Residual',
+                 x=paste0("Predictor (`", predictor,"`)"))
+    }
+}
